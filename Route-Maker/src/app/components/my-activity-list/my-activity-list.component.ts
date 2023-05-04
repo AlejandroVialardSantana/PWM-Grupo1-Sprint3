6947c455f67fc665a5daefd4d58eacf5ed1ac771
unknown <alejandrovialard2002@gmail.com>
2023-05-04 14:52:57 +0000

    Mostrar actividades en la lista de usuario

--- Route-Maker/src/app/components/my-activity-list/my-activity-list.component.ts
+++ Route-Maker/src/app/components/my-activity-list/my-activity-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { UsersService } from 'src/app/services/users.service';
-
+import { Actividad } from '../../models/interfaces/actividades';
 
 @Component({
   selector: 'app-my-activity-list',
@@ -9,4 +9,16 @@ import { UsersService } from 'src/app/services/users.service';
 })
 export class MyActivityListComponent {
 
+  activities: Actividad[] = [];
+  user$ = this.usersService.currentUserProfile$;
+
+  constructor(private usersService: UsersService) { }
+
+  ngOnInit(): void {
+    this.user$.subscribe(user => {
+      if(user){
+        this.activities = user.activities || [];
+      }
+    });
+  }
 }
