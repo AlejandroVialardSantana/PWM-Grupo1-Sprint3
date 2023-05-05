debae22087012e5adcfe7acf0b4c576efaec2bae
anaMariaJimenezPerez <90760700+anaMJimenezPerez@users.noreply.github.com>
2023-05-05 22:55:40 +0100

    Implementado eliminar usuario

--- Route-Maker/src/app/components/change-password/change-password.component.ts
+++ Route-Maker/src/app/components/change-password/change-password.component.ts
@@ -1,5 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { OpenOverlayService } from 'src/app/services/open-overlay.service';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators, ValidatorFn } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { switchMap } from 'rxjs';
+import { UsersService } from 'src/app/services/users.service';
+import { user } from '@angular/fire/auth';
+
+export function passwordsMatch(): ValidatorFn{
+  return (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    if(password && confirmPassword && password !== confirmPassword){
+      return {
+        passwordsDontMatch: true
+      }
+    }
+    return null;
+  };
+}
 
 @Component({
   selector: 'app-change-password',
@@ -8,9 +29,34 @@ import { OpenOverlayService } from 'src/app/services/open-overlay.service';
 })
 export class ChangePasswordComponent implements OnInit{
 
-  constructor(public openOverlayService: OpenOverlayService){}
+  user$ = this.usersService.currentUserProfile$;
+
+  constructor(
+    public openOverlayService: OpenOverlayService,
+    private usersService: UsersService,
+    private authService: AuthenticationService
+    ){}
+
+  changePasswordForm = new FormGroup({
+    Password: new FormControl('', [Validators.required ]),
+    newPassword: new FormControl('', [Validators.minLength(8), Validators.required ]),
+    repeatPassword: new FormControl('', [Validators.required])
+  },
+    { validators: passwordsMatch() }
+  )
+
+  get password(){
+    return this.changePasswordForm.get('Password');
+  }
+  get newPassword(){
+    return this.changePasswordForm.get('newPassword');
+  }
+  get repeatPassword(){
+    return this.changePasswordForm.get('repeatPassword');
+  }
 
   ngOnInit(): void {
 
   }
+
 }
--- Route-Maker/src/app/services/users.service.ts
+++ Route-Maker/src/app/services/users.service.ts
@@ -1,36 +1,67 @@
 import { Injectable } from '@angular/core';
-import { Firestore, doc, setDoc, docData, updateDoc } from '@angular/fire/firestore';
+import {
+  Firestore,
+  doc,
+  setDoc,
+  docData,
+  updateDoc,
+} from '@angular/fire/firestore';
 import { ProfileUser } from '../models/user-profile';
 import { from, Observable, switchMap, of } from 'rxjs';
 import { AuthenticationService } from './authentication.service';
+import { deleteDoc } from 'firebase/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
-
-  get currentUserProfile$(): Observable<ProfileUser | null>{
+  get currentUserProfile$(): Observable<ProfileUser | null> {
     return this.authService.currentUser$.pipe(
-      switchMap(user => {
-        if(!user?.uid){
+      switchMap((user) => {
+        if (!user?.uid) {
           return of(null);
         }
 
         const ref = doc(this.firestore, 'users', user?.uid);
         return docData(ref) as Observable<ProfileUser>;
       })
-    )
+    );
   }
 
-  constructor(private firestore: Firestore, private authService: AuthenticationService) { }
+  constructor(
+    private firestore: Firestore,
+    private authService: AuthenticationService,
+    private db: AngularFirestore
+  ) {}
 
-  addUser(user: ProfileUser ): Observable<any>{
+  getAllUsers() {
+    return new Promise<any>((resolve) => {
+      this.db
+        .collection('users')
+        .valueChanges({ idField: 'id' })
+        .subscribe((users) => resolve(users));
+    });
+  }
+
+  addUser(user: ProfileUser): Observable<any> {
     const ref = doc(this.firestore, 'users', user?.uid);
     return from(setDoc(ref, user));
   }
-  
+
   updateUser(user: ProfileUser): Observable<void> {
     const ref = doc(this.firestore, 'users', user.uid);
     return from(updateDoc(ref, { ...user }));
+  }
+
+  deleteUser(user: ProfileUser): Observable<any> {
+    const ref = doc(this.firestore, `users/${user.uid}`);
+    return from(deleteDoc(ref));
+  }
+
+  changePassword(user: ProfileUser): Observable<void> {
+    const ref = doc(this.firestore, 'users', user.uid);
+    return from(updateDoc(ref, { user: user.password }));
   }
 }
